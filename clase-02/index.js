723bc06ff461190eff68e0c73fca527704869e45
roger17 <roger17@git.com>
2026-03-04 20:09:00 +0100

    actualizando

--- clase-02/index.js
+++ clase-02/index.js
@@ -1,31 +1,8 @@
-// import * as http from "node:http";
-
-// const server = http.createServer((request, response) => {
-//   if (request.url == "/ping") {
-//     response.statusCode = 200;
-//     response.setHeader("Content-Type", "application/json");
-//     response.end(JSON.stringify({ message: "pong" }));
-//     return;
-//   }
-
-//   response.statusCode = 404;
-//   response.setHeader("Content-Type", "application/json");
-//   response.end(JSON.stringify({ error: "Not Found" }));
-// });
-
-// server.listen(3000, () => console.log("http://localhost:3000"));
-
 import express from "express";
-import productsRouter from "./routes/products.router";
-import categoriesRouter from "./routes/categories.router";
-import pingRouter from "./routes/ping.router";
 
 const app = express();
 
 app.use(express.json());
-app.use("/products", productsRouter);
-app.use("/categories", categoriesRouter);
-app.use("/ping", pingRouter);
 
 const products = [
   { id: 1, name: "Laptop", price: 1200 },
@@ -33,15 +10,178 @@ const products = [
 ];
 
 const categories = [
-  { id: 1, name: "Electronics", description: "Electronic devices" },
-  { id: 2, name: "Accessories", description: "Device accessories" },
+  {
+    id: 1,
+    name: "Electro",
+    description: "Lorem ipsum",
+  },
+  {
+    id: 2,
+    name: "Bazar",
+    description: "Lorem ipsum bazar",
+  },
 ];
 
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
+app.get("/products/:id", (req, res) => {
+  // const id = Number(req.params.id);
+  const id = parseInt(req.params.id);
 
+  // console.log(typeof req.params.id, typeof id, id, isNaN(id));
 
-// app.get("/ping", (req, res) => {
-//   res.json({ message: "pong" });
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  const product = products.find((p) => p.id == id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  res.json(product);
+});
+
+app.post("/products", (req, res) => {
+  // console.log(req.body);
+
+  // console.log(Date.now());
+
+  const newProduct = {
+    id: Date.now(),
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+app.get("/categories", (req, res) => {
+  res.json(categories);
+});
+
+// app.get("/categorias/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+
+//   if (isNaN(id)) {
+//     return res.status(400).json({ error: "Error de usuario" });
+//   }
+
+//   const categoria = categories.find((cat) => cat.id == id);
+
+//   if (!categoria) {
+//     return res.status(404).json({ error: "ERROR" }); // esto es el error 404 (no se encuentra)
+//   }
+
+//   res.json(categoria);
 // });
+
+// app.get("/categories/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+
+//   if (isNaN(id)) {
+//     return res.status(400).json({ error: "Invalid category" });
+//   }
+
+//   const category = categories.find((cat) => cat.id == id);
+
+//   if (!category) {
+//     return res.status(404).json({ error: "category not found" });
+//   }
+
+//   res.json(category);
+// });
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+
+  const category = categories.find((c) => c.id == req.params.id);
+
+  if (!category) {
+    return res.status(404).json({ message: "Category not found" });
+  }
+
+  res.json(category);
+});
+
+// app.post("/categorias", (req, res) => {
+//   // console.log(req.body);
+//   // res.send("ok");
+//   const newCategory = {
+//     id: Date.now(),
+//     name: req.body.name,
+//   };
+
+//   categories.push(newCategory);
+
+//   res.status(201).json(newCategory);
+// });
+
+// app.post("/categories", (req, res) => {
+//   const newCategory = {
+//     id: Date.now(),
+//     name: req.body.name,
+//     description: req.body.description,
+//   };
+
+//   categories.push(newCategory);
+
+//   res.status(201).json(newCategory);
+// });
+
+// app.post("/categories", (req, res) => {
+//   //console.log(req.body);
+//   const newCategory = {
+//     id: Date.now(),
+//     name: req.body.name,
+//   };
+
+//   categories.push(newCategory);
+
+//   res.status(201).json(newCategory);
+// });
+
+app.post("/categories", (req, res) => {
+  console.log(req.body, req.body.name);
+
+  // if (!req.body.name) {
+  //   return res.status(422).json({ error: "name is required" });
+  // }
+
+  // if (req.body.name == undefined) {
+  //   return res.status(422).json({ error: "name is required" });
+  // }
+
+  // if (req.body.name == "") {
+  //   return res.status(422).json({ error: "name is required" });
+  // }
+
+  if (req.body.name == undefined || req.body.name == "") {
+    return res.status(422).json({ error: "name is required" });
+  }
+
+  const newCategory = {
+    id: Date.now(),
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  categories.push(newCategory);
+
+  res.status(201).json(newCategory);
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
 
 app.listen(3000, () => console.log("http://localhost:3000"));
--- clase-03/index.js
+++ clase-03/index.js
@@ -16,10 +16,16 @@
 // server.listen(3000, () => console.log("http://localhost:3000"));
 
 import express from "express";
+import productsRouter from "./routes/products.router";
+import categoriesRouter from "./routes/categories.router";
+import pingRouter from "./routes/ping.router";
 
 const app = express();
 
 app.use(express.json());
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/ping", pingRouter);
 
 const products = [
   { id: 1, name: "Laptop", price: 1200 },
@@ -31,82 +37,11 @@ const categories = [
   { id: 2, name: "Accessories", description: "Device accessories" },
 ];
 
-app.get("/products", (req, res) => {
-  res.json(products);
-});
 
-app.get("/products/:id", (req, res) => {
-  // const id = Number(req.params.id);
-  const id = parseInt(req.params.id);
 
-  // console.log(typeof req.params.id, typeof id, id, isNaN(id));
 
-  if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid product ID" });
-  }
-
-  const product = products.find((p) => p.id === id);
-
-  if (!product) {
-    return res.status(404).json({ error: "Product not found" });
-  }
-
-  res.json(product);
-});
-
-app.post("/products", (req, res) => {
-  const newProduct = {
-    id: Date.now(),
-    name: req.body.name,
-    price: req.body.price,
-  };
-
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-
-app.get("/categories", (req, res) => {
-  res.json(categories);
-});
-
-app.get("/categories/:id", (req, res) => {
-  // const id = Number(req.params.id);
-  const id = parseInt(req.params.id);
-
-  // console.log(typeof req.params.id, typeof id, id, isNaN(id));
-
-  if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid category ID" });
-  }
-
-  const category = categories.find((c) => c.id === id);
-
-  if (!category) {
-    return res.status(404).json({ error: "Category not found" });
-  }
-
-  res.json(category);
-});
-
-app.post("/categories", (req, res) => {
-  console.log(req.body);
-
-  if (req.body.name == undefined || req.body.name == "") {
-    return res.status(422).json({ error: "name is required" });
-  }
-
-  const newCategory = {
-    id: Date.now(),
-    name: req.body.name,
-    description: req.body.description,
-  };
-
-  categories.push(newCategory);
-  res.status(201).json(newCategory);
-});
-
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
-});
+// app.get("/ping", (req, res) => {
+//   res.json({ message: "pong" });
+// });
 
 app.listen(3000, () => console.log("http://localhost:3000"));
