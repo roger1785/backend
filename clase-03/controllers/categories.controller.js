595ddb7f55cd5ac1c21b14ea249a5c17044589ee
roger17 <roger17@git.com>
2026-03-04 21:13:30 +0100

    actualizando

---
+++ clase-03/controllers/categories.controller.js
@@ -0,0 +1,54 @@
+const categories = [
+  { id: 1, name: "Electronics", description: "Electronic devices" },
+  { id: 2, name: "Accessories", description: "Device accessories" },
+];
+
+
+export const getCategories = (req, res) => {
+  res.json(categories);
+};
+
+export const getCategoryById = (req, res) => {
+  // const id = Number(req.params.id);
+  const id = parseInt(req.params.id);
+
+  // console.log(typeof req.params.id, typeof id, id, isNaN(id));
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid category ID" });
+  }
+
+  const category = categories.find((c) => c.id === id);
+
+  if (!category) {
+    return res.status(404).json({ error: "Category not found" });
+  }
+
+  res.json(category);
+}
+
+  // console.log(typeof req.params.id, typeof id, id, isNaN(id));
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid product ID" });
+  }
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  res.json(product);
+;
+
+export const createCategory = (req, res) => {
+  const newCategory = {
+    id: Date.now(),
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  categories.push(newCategory);
+  res.status(201).json(newCategory);
+};
