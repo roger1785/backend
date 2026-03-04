50cf4dc34177dce9668f95f4827a3b861fb22f1f
roger17 <roger17@git.com>
2026-03-04 21:02:46 +0100

    actualizando ficheros

--- clase-03/controllers/products.controller.js
+++ clase-03/controllers/products.controller.js
@@ -1,7 +1,11 @@
+const products = [
+  { id: 1, name: "Laptop", price: 1200 },
+  { id: 2, name: "Mouse", price: 20 },
+];
+
 export const getProducts = (req, res) => {
   res.json(products);
 };
-
 
 export const getProductById = (req, res) => {
   // const id = Number(req.params.id);
@@ -20,9 +24,15 @@ export const getProductById = (req, res) => {
   }
 
   res.json(product);
-}
+};
 
-const products = [
-  { id: 1, name: "Laptop", price: 1200 },
-  { id: 2, name: "Mouse", price: 20 },
-];  
+export const createProduct = (req, res) => {
+  const newProduct = {
+    id: Date.now(),
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+};
--- clase-03/routes/products.router.js
+++ clase-03/routes/products.router.js
@@ -5,26 +5,13 @@ const router = Router();
 import {
   getProductById,
   getProducts,
+  createProduct,
 } from "../controllers/products.controller.js";
-
-const products = [
-  { id: 1, name: "Laptop", price: 1200 },
-  { id: 2, name: "Mouse", price: 20 },
-];
 
 router.get("/", getProducts);
 
 router.get("/:id", getProductById);
 
-router.post("/", (req, res) => {
-  const newProduct = {
-    id: Date.now(),
-    name: req.body.name,
-    price: req.body.price,
-  };
-
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
+router.post("/", createProduct );
 
 export default router;
